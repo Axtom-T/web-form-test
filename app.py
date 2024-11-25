8478bacb20c320b039baa61a08376c28d72274f7
Axtom-T <135135832+Axtom-T@users.noreply.github.com>
2024-11-25 15:15:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-#TODO: put DB outputs into templates and display properly
-#https://github.com/Axtom-T/Bright_Backer/blob/main/backend/views/donation_history.tpl
+#TODO: insert values for new tables and test displays
 import os
 from flask_migrate import Migrate
 from flask import (Flask, redirect, render_template, request,
@@ -25,6 +24,42 @@ class Devices(db.Model):
     __tablename__ = 'devices'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String)
+    type = db.Column(db.String)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    FAT_status = db.Column(db.String)
+    FAT_test_id = db.Column(db.Integer)
+    SAT_status = db.Column(db.String)
+    SAT_test_id = db.Column(db.Integer)
+    SIAT_status = db.Column(db.String)
+    SIAT_test_id = db.Column(db.Integer)
+    FST_status = db.Column(db.String)
+    FST_test_id = db.Column(db.Integer)
+    project_number = db.Column(db.Integer)
+
+class CCTV_SIAT_results(db.Model):
+    __tablename__ = 'devices'
+    test_id = db.Column(db.Integer, primary_key=True)
+    asset_id = db.Column(db.Integer)
+    test_date = db.Column(db.DateTime)
+    pingable = db.Column(db.Boolean)
+    ping_latency_expected = db.Column(db.Boolean)
+    ping = db.Column(db.Integer)
+    web_access = db.Column(db.Boolean)
+    FLIR_discover = db.Column(db.Boolean)
+    video_settings = db.Column(db.Boolean)
+    focus = db.Column(db.Boolean)
+    clean_display = db.Column(db.Boolean)
+    picture_quality = db.Column(db.Boolean)
+    PTZ_function = db.Column(db.Boolean)
+    PTZ_privacy = db.Column(db.Boolean)
+    washer = db.Column(db.Boolean)
+    preset = db.Column(db.Boolean)
+    recording = db.Column(db.Boolean)
+    webcam_position = db.Column(db.Boolean)
+    firmware_version = db.Column(db.String)
+    record_diagnostics = db.Column(db.Boolean)
+    SIAT_pass = db.Column(db.String)
 
 @app.route('/')
 def index():
